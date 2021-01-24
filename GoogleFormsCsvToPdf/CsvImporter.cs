1cc76f77fedf0e6e49ab92fbd04e5cbb490e96d7
Merlijn Vanherck <merlijnvanherck@gmail.com>
2021-01-24 16:25:07 +0100

    Created CsvImporter class with method to read a csv as FormData object

---
+++ GoogleFormsCsvToPdf/CsvImporter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Csv;
+
+namespace GoogleFormsCsvToPdf
+{
+    static class CsvImporter
+    {
+        public static FormData ReadCsv(Stream fileStream)
+        {
+            var csv = CsvReader.ReadFromStream(fileStream);
+
+            var formFields = new List<FormField>();
+
+            foreach (var field in csv.First().Headers)
+                formFields.Add(new FormField(field));
+
+            var formData = new FormData(formFields.ToArray());
+
+            foreach (var row in csv)
+                formData.AddRecord(row.Values);
+
+            return formData;
+        }
+    }
+}
